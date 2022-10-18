4c127e6954caf52d2691f00226d8c43f4692d3e2
Louisss-Lam <louislam617@gmail.com>
2022-10-18 21:22:48 +0100

    add rapidAPI

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '81f32d6c58mshed2a37e181da90cp1e8d90jsn97e5e3750466')
+            
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
