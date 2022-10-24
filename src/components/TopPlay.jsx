e1c14186bf8882321b0342fc0ac582cde96d8901
Louisss-Lam <louislam617@gmail.com>
2022-10-24 11:51:17 +0100

    finish

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -61,7 +61,7 @@ const TopPlay = () => {
 
   return(
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-6 flex-1 
-    xl:max-w[500px] max-w-full flex flex-col">
+    xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
         <div className="flex flex-col justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Charts</h2>
